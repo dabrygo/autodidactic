191b34676d55639f8085e723a0f52cafc9c3357f
Daniel <dbgodfrey89@msn.com>
2020-01-26 15:17:58 -0700

    Form questions that are false

--- q_and_a.py
+++ q_and_a.py
@@ -4,14 +4,14 @@ import random
 
 
 class Fact:
-    def __init__(self, facts, key, attr):
-        self._facts = facts
+    def __init__(self, fact, key, attr):
+        self._fact = fact
         self._key = key
         self._attr = attr
 
     def question(self):
-        key = self._facts[self._key]
-        attr = self._facts[self._attr]
+        key = self._fact[self._key]
+        attr = self._fact[self._attr]
         return f"{attr} is the {self._attr} of {key}"
 
     def is_true(self):
@@ -19,6 +19,25 @@ class Fact:
 
     def is_false(self):
         return False
+
+
+class Fiction:
+    def __init__(self, fact, other_fact, key, attr):
+        self._fact = fact
+        self._other_fact = other_fact
+        self._key = key
+        self._attr = attr
+
+    def question(self):
+        key = self._fact[self._key]
+        attr = self._other_fact[self._attr]
+        return f"{attr} is the {self._attr} of {key}"
+
+    def is_true(self):
+        return False
+
+    def is_false(self):
+        return True
 
 
 states = []
@@ -33,20 +52,27 @@ n = 50
 correct = 0
 keys = states
 for i in range(n):
-    choice = random.choice(keys)
-    fact = Fact(choice, 'state', 'capital')
+    fact = random.randint(0, 1)
+    if fact:
+        choice = random.choice(keys)
+        question = Fact(choice, 'state', 'capital')
+    else:
+        choice = random.choice(keys)
+        choice2 = random.choice(keys)
+        question = Fiction(choice, choice2, 'state', 'capital')
 
+    print()
     print(f"Question {i}/{n}")
-    print("True or False.", fact.question())
+    print("True or False.", question.question())
     answer = input(">>> ").strip()
     while answer not in ['T', 'F']:
         print(f"Invalid answer '{answer}'. Please type T or F.")
-        print("True or False:", fact.question())
+        print("True or False:", question.question())
         answer = input(">>> ").strip()
 
-    if answer == 'T' and fact.is_true():
+    if answer == 'T' and question.is_true():
         correct += 1
-    elif answer == 'F' and fact.is_false():
+    elif answer == 'F' and question.is_false():
         correct += 1
     else:
         print("Nope.")
